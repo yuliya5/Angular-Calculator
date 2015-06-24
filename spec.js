2af81183140ecbf888e2e7e7c8a08bbb4093a219
yuliya5 <ysuvorava@gmail.com>
2015-06-24 15:42:50 -0600

    Buttons updated

---
+++ spec.js
@@ -0,0 +1,9 @@
+describe('functional financial calculator', function() {
+  it('should add together numbers', function() {});
+  it('should have a display window', function() {});
+  it('should resize according to the window size', function() {});
+  it('should handle numbers with decimals', function() {});
+
+ });
+
+
